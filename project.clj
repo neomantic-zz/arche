b95efe8f95ebf4af9c2318085d292818e12c416f
Chad Albers <calbers@neomantic.com>
2014-08-29 15:11:06 +0900

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject wormhole-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject wormhole-clj "0.0.1-SNAPSHOT"
+  :description "Resource Catalog Hypermedia Service"
+  :url "http://bitbucket.org/calbers"
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot wormhole-clj.core
   :target-path "target/%s"
