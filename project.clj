2417baddcb18a39c197678e771ddb53ce9777ce7
Chad Albers <calbers@neomantic.com>
2014-08-29 15:12:09 +0900

    added liberator

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject wormhole-clj "0.0.1-SNAPSHOT"
   :description "Resource Catalog Hypermedia Service"
   :url "http://bitbucket.org/calbers"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [liberator "0.12.1"]]
   :main ^:skip-aot wormhole-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
