cfd7da6b4ec5846953be332b6f7bb64b304a1c72
Chad Albers <calbers@neomantic.com>
2014-08-29 17:45:18 +0900

    working response

--- project.clj
+++ project.clj
@@ -2,7 +2,11 @@
   :description "Resource Catalog Hypermedia Service"
   :url "http://bitbucket.org/calbers"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [liberator "0.12.1"]]
+                 [liberator "0.12.1"]
+                 [compojure "1.1.8"]
+                 [ring/ring-core "1.3.1"]
+                 [ring/ring-jetty-adapter "1.3.0"]]
+  :dev-dependencies [[ring-serve "0.1.2"]]
   :main ^:skip-aot wormhole-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/wormhole_clj/core.clj
+++ src/wormhole_clj/core.clj
@@ -1,7 +1,16 @@
 (ns wormhole-clj.core
-  (:gen-class))
+  (:require [liberator.core :refer [resource defresource]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes ANY]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defresource discoverable-resource [resource-name]
+  :available-media-types ["application/json"]
+  :handle-ok (fn [_] (format "Returning %s" resource-name)))
+
+(defroutes app
+  (ANY "/v1/discoverable_resources/:resource-name" [resource-name]
+       (discoverable-resource resource-name)))
+
+(def handler
+  (-> app
+      wrap-params))
