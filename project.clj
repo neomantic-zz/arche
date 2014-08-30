56db31fb31107c5c2014546492abe1bd0e5f27aa
Chad Albers <calbers@neomantic.com>
2014-08-30 19:19:17 +0900

    fixed problem starting ring

--- project.clj
+++ project.clj
@@ -6,10 +6,9 @@
                  [compojure "1.1.8"]
                  [ring/ring-core "1.3.1"]
                  [ring/ring-jetty-adapter "1.3.0"]]
-  :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[ring-serve "0.1.2"]
-                                  [speclj "3.1.0"]]}}
-  :plugins [[lein-ring "0.8.7"]
+  :profiles {:uberjar {:aot :all}}
+  :plugins [[ring-serve "0.1.2"]
+            [lein-ring "0.8.11"]
             [speclj "3.1.0"]]
   :main ^:skip-aot wormhole-clj.core
   :ring {:handler wormhole-clj.core/handler}
