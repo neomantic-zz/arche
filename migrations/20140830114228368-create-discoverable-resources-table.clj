614f20b2b3a7ecbed561c8b39c1c07c70438fb88
Chad Albers <calbers@neomantic.com>
2014-08-30 15:28:25 +0900

    removed the datetimes and index from migration...not ready to support them

--- migrations/20140830114228368-create-discoverable-resources-table.clj
+++ migrations/20140830114228368-create-discoverable-resources-table.clj
@@ -1,8 +1,7 @@
 ;; migrations/20140830114228368-create-discoverable-resources-table.clj
 
 (defn up []
-  ["CREATE TABLE discoverable_resources(id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, resource_name VARCHAR(255) NOT NULL, link_relation VARCHAR(255) NOT NULL, href VARCHAR(255) NOT NULL, created_on DATETIME NOT NULL, updated_on DATETIME NOT NULL)"
-   "CREATE INDEX id_index ON discoverable_resources (id)"])
+  ["CREATE TABLE discoverable_resources(id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, resource_name VARCHAR(255) NOT NULL, link_relation VARCHAR(255) NOT NULL, href VARCHAR(255) NOT NULL)"])
 
 (defn down []
   ["DROP TABLE discoverable_resources"])
