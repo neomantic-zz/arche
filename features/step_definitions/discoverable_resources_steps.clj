32ea350572ac9240818f83b436e5c6095968de92
Chad Albers <calbers@neomantic.com>
2014-09-06 21:50:31 +0900

    steps that are almost passing

--- features/step_definitions/discoverable_resources_steps.clj
+++ features/step_definitions/discoverable_resources_steps.clj
@@ -2,6 +2,7 @@
   (:use wormhole-clj.core wormhole-clj.db cucumber.runtime.clj clojure.test)
   (:require [clojure.java.jdbc :as jdbc]
             [ring.adapter.jetty :as ring]
+            [wormhole-clj.media :as media]
             [cheshire.core :refer :all :as json]
             [clj-http.client :as client]))
 
@@ -14,17 +15,41 @@
 (defn last-response-set! [new-response]
   (reset! last-response new-response))
 
+(defn execute-get-request [path headers]
+  (client/get (format "%s:%d/%s" "http://localhost" test-port path)
+              {:headers headers}))
 
-;; borrowed from here.
-;;https://github.com/ring-clojure/ring/blob/master/ring-jetty-adapter/test/ring/adapter/test/jetty.clj#L25
-(defmacro with-server [app options & body]
-  `(let [server# (ring/run-jetty ~app ~(assoc options :join? false))]
-     (try
-       ~@body
-       (finally (.stop server#)))))
+(defn verify-app-url [url]
+  (when-let [result (re-matches
+                   (re-pattern
+                    (format "\\A%s(.*)" (java.util.regex.Pattern/quote (base-uri))))
+                   url)]
+    (let [response (execute-get-request
+                    (let [path (nth result 1)]
+                      (if (= "" path) "/" path)))]
+      (is (= 200 (:status response))))))
+
+(def server (atom nil))
+
+(defn server-start []
+  (if @server
+    (throw (IllegalStateException. "Server already started."))
+    (reset! server (ring/run-jetty handler
+                                   {:port test-port
+                                    :join? false}))))
+
+(defn server-stop []
+  (if (nil? @server)
+    (throw (IllegalStateException. "Server already stopped."))
+    (do
+      (.stop @server)
+      (reset! server nil))))
+
+(Before [] (server-start))
 
 (After []
        (reset! last-response nil)
+       (server-stop)
        (jdbc/db-do-commands dbspec "TRUNCATE TABLE discoverable_resources;"))
 
 (Given #"^a discoverable resource exists with the following attributes:$" [table]
@@ -35,16 +60,15 @@
           (get table-map "href"))))
 
 (When #"^I invoke the uniform interface method GET to \"([^\"]*)\" accepting \"([^\"]*)\"$" [path media-type]
-      (with-server handler {:port test-port}
-        (last-response-set!
-         (client/get (format "%s:%d/%s" "http://localhost" test-port path)
-                     {:headers {"Accept" media-type}}))))
+      (last-response-set!
+       (client/get (format "%s:%d/%s" "http://localhost" test-port path)
+                   {:headers {"Accept" media-type}})))
 
 (Then #"^I should get a status of (\d+)$" [status]
       (is (= (:status @last-response) (read-string status))))
 
 (Then #"^the resource representation should have exactly the following properties:$" [table]
-      (let [actual (into {} (remove (fn [[key item]] (= key :_links))
+      (let [actual (into {} (remove (fn [[key item]] (= key media/keyword-links))
                                     (json/parse-string (:body @last-response) true)))
             map-of-table (table-to-map table)
             expected (zipmap
@@ -53,16 +77,17 @@
         (is (= expected actual))))
 
 (Then #"^the resource representation should have exactly the following links:$" [table]
-      (let [actual-links (get (json/parse-string (:body @last-response)) "_links")
+      (let [actual-links (get (json/parse-string (:body @last-response)) (name media/keyword-links))
             expected-links (into {} (rest (table-to-map table)))]
         (is (= (count expected-links)
                (count actual-links)))
         (is (= (keys expected-links)
                (keys actual-links)))
+        (prn actual-links)
         (doall
          (map (fn [link]
                 (let [[link-relation href] link]
-                  (= (get actual-links link-relation)
-                     href)))
+                  (is (= (get-in actual-links [link-relation (name media/keyword-href)])))
+                  (verify-app-url href)))
               expected-links))
         ))
