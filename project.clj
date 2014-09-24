9a0f807eaadc486f45c39d58311a6f742fb7344a
Chad Albers <calbers@neomantic.com>
2014-09-24 23:41:09 +0900

    updated project url

--- project.clj
+++ project.clj
@@ -18,8 +18,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defproject arche "0.0.1-SNAPSHOT"
-  :description "Resource Catalog Hypermedia Service"
-  :url "http://bitbucket.org/calbers"
+  :description "A hypermedia resource discovery service"
+  :url "https://github.com/neomantic/arche"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [liberator "0.12.1"]
                  [compojure "1.1.8"]
