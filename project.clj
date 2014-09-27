b8d5f67f45c39c70720f1ad5d1bd04cf009f79ba
Chad Albers <calbers@neomantic.com>
2014-09-27 18:19:20 +0900

    added name for jar

--- project.clj
+++ project.clj
@@ -60,6 +60,7 @@
   :clj-sql-up {:database {:subprotocol "mysql"}
                :deps [[mysql/mysql-connector-java "5.1.25"]]}
   :min-lein-version "2.0.0"
+  :uberjar-name "arche-standalone.jar"
   :main ^:skip-aot arche.core
   :ring {:handler arche.core/handler}
   :test-paths ["spec"]
