878314500341deeaccdc64362535bc2bd92abcc8
Chad Albers <calbers@neomantic.com>
2014-12-30 17:47:22 +0900

    document for arche.http

--- src/arche/http.clj
+++ src/arche/http.clj
@@ -20,18 +20,37 @@
 (ns arche.http
   (:require [pandect.core :refer [md5]]))
 
+;; arche.http contains helps function for managing http headers
+
 (defn- make-header-fn [key]
   (fn [content]
     {key content}))
 
-(def header-location (make-header-fn "Location"))
-(def header-accept (make-header-fn "Accept"))
-(def header-cache-control (make-header-fn "Cache-Control"))
-(def header-etag (make-header-fn "ETag"))
-(def header-content-type (make-header-fn "Content-Type"))
+(def header-location
+  "Returns a hash-map with 'Location' as its key and the parameter set as its value"
+  (make-header-fn "Location"))
+
+(def header-accept
+  "Returns a hash-map with 'Accept' as its key and the parameter set as its value"
+  (make-header-fn "Accept"))
+
+(def header-cache-control
+  "Returns a hash-map with 'Cache-Control' as its key and the parameter set as its value"
+  (make-header-fn "Cache-Control"))
+
+(def header-etag
+  "Returns a hash-map with 'Etag' as its key and the parameter set as its value"
+  (make-header-fn "ETag"))
+
+(def header-content-type
+  "Returns a hash-map with 'Content-Type' as its key and the parameter set as its value"
+  (make-header-fn "Content-Type"))
 
 (defn cache-control-header-private-age [number]
+  "Returns a hash-map with the key set as Cache-Control, and the value
+   set to private, and with a max-age equal to the value of the parameter"
   (header-cache-control (format "max-age=%d, private" number)))
 
 (defn etag-make [string]
+  "Given a string, return a md5 hash intended to me used for an etag"
   (md5 string))
